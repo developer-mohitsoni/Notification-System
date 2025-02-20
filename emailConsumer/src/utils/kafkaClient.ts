1435c347af53fbfa464509f7f97f73418638c0d8
Mohit Soni <developer_mohitsoni@outlook.com>
2025-02-21 00:41:44 +0530

    Add KafkaClient singleton to manage Kafka connection

---
+++ emailConsumer/src/utils/kafkaClient.ts
@@ -0,0 +1,19 @@
+import { Kafka } from "kafkajs";
+
+class KafkaClient {
+  private static instance: Kafka;
+
+  private constructor() {}
+
+  public static getInstance(): Kafka {
+    if (!KafkaClient.instance) {
+      KafkaClient.instance = new Kafka({
+        clientId: "emailInit",
+        brokers: [process.env.KAFKA_BROKER!],
+      });
+    }
+    return KafkaClient.instance;
+  }
+}
+
+export default KafkaClient;
