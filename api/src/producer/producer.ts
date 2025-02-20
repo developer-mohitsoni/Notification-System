a4640da5bdbb62ae4e1b477ee5943e2d6e3aebb5
Mohit Soni <developer_mohitsoni@outlook.com>
2025-02-20 18:11:42 +0530

    Add KafkaService for sending messages with singleton pattern

---
+++ api/src/producer/producer.ts
@@ -0,0 +1,47 @@
+import { Kafka, Producer } from "kafkajs";
+import "dotenv.config";
+
+interface SendMessage {
+  topic: string;
+  message: string;
+  to: string;
+}
+
+class KafkaService {
+  private static instance: KafkaService;
+  private kafka: Kafka;
+  private producer: Producer;
+
+  private constructor() {
+    this.kafka = new Kafka({
+      clientId: "notification-service",
+      brokers: [process.env.KAFKA_BROKERS!],
+    });
+    this.producer = this.kafka.producer();
+  }
+  static getInstance(): KafkaService {
+    if (!KafkaService.instance) {
+      KafkaService.instance = new KafkaService();
+    }
+    return KafkaService.instance;
+  }
+  async sendMessage({ topic, message, to }: SendMessage): Promise<void> {
+    await this.producer.connect();
+    await this.producer.send({
+      topic,
+      messages: [
+        {
+          value: JSON.stringify({
+            to,
+            message,
+          }),
+        },
+      ],
+    });
+    console.log(`📤 Message sent to ${topic}: ${message}`);
+  }
+}
+
+// ✅ Singleton Instance
+const kafkaService = KafkaService.getInstance();
+export { kafkaService };
