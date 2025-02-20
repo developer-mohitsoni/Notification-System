ded0df46b2c5bfdb2dba770dc6ec88aee664f94b
Mohit Soni <developer_mohitsoni@outlook.com>
2025-02-21 01:11:54 +0530

    Add KafkaConsumerService to handle message consumption from Kafka

---
+++ emailConsumer/src/services/kafkaConsumerService.ts
@@ -0,0 +1,40 @@
+import KafkaClient from "../utils/kafkaClient";
+
+class KafkaConsumerService {
+  private static instance: KafkaConsumerService;
+  private consumer = KafkaClient.getInstance().consumer({
+    groupId: "email-group",
+  });
+
+  private constructor() {}
+
+  public static getInstance(): KafkaConsumerService {
+    if (!KafkaConsumerService.instance) {
+      KafkaConsumerService.instance = new KafkaConsumerService();
+    }
+    return KafkaConsumerService.instance;
+  }
+
+  public async consumeMessages(
+    topic: string,
+    callback: (message: any) => void
+  ): Promise<void> {
+    await this.consumer.connect();
+    await this.consumer.subscribe({ topic, fromBeginning: true });
+
+    await this.consumer.run({
+      eachMessage: async ({ topic, partition, message }) => {
+        console.log({
+          partition,
+          offset: message.offset,
+          value: message.value?.toString(),
+        });
+
+        const parsedData = JSON.parse(message.value?.toString() || "{}");
+        callback(parsedData); // Message milne par callback function call hoga
+      },
+    });
+  }
+}
+
+export default KafkaConsumerService;
