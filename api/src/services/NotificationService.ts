923bee30188b3a8ace835a83baa28e246a088e5d
Mohit Soni <developer_mohitsoni@outlook.com>
2025-02-20 18:11:49 +0530

    Integrate Kafka messaging in NotificationService for sending notifications via email, SMS, and WhatsApp

--- api/src/services/NotificationService.ts
+++ api/src/services/NotificationService.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from "@prisma/client";
+import { kafkaService } from "../producer/producer.js";
 
 export class NotificationService {
   private prisma: PrismaClient;
@@ -8,20 +9,57 @@ export class NotificationService {
   }
 
   async sendNotification(userId: number, message: string) {
-    // Fetch user and notification preferences
-    const [user, preferences] = await Promise.all([
-      this.prisma.user.findUnique({
-        where: { id: userId },
-      }),
-      this.prisma.notificationPreferences.findUnique({
-        where: { userId: userId },
-      }),
-    ]);
+    try {
+      // Fetch user and notification preferences
+      const [user, notificationPreference] = await Promise.all([
+        this.prisma.user.findUnique({
+          where: { id: userId },
+        }),
+        this.prisma.notificationPreferences.findUnique({
+          where: { userId: userId },
+        }),
+      ]);
 
-    if (!user || !preferences) {
-      throw new Error("User or notification preference not found");
+      if (!user || !notificationPreference) {
+        throw new Error("User or notification preference not found");
+      }
+
+      const promises: Promise<void>[] = [];
+
+      if (notificationPreference.email) {
+        promises.push(
+          kafkaService.sendMessage({
+            topic: "email",
+            message,
+            to: user.email,
+          })
+        );
+      }
+      if (notificationPreference.sms) {
+        promises.push(
+          kafkaService.sendMessage({
+            topic: "sms",
+            message,
+            to: user.mobileNumber,
+          })
+        );
+      }
+      if (notificationPreference.whatsapp) {
+        promises.push(
+          kafkaService.sendMessage({
+            topic: "whatsapp",
+            message,
+            to: user.mobileNumber,
+          })
+        );
+      }
+
+      // Wait for all notifications to be sent
+      await Promise.all(promises);
+
+      return { message: "Notification sent successfully" };
+    } catch {
+      throw new Error("Error sending notification:");
     }
-
-    return { message: "Notification sent successfully" };
   }
 }
