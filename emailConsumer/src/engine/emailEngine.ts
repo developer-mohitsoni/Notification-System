550ebbeecf325704588f12e9868cc0182d95e118
Mohit Soni <developer_mohitsoni@outlook.com>
2025-02-21 00:34:20 +0530

    Add EmailEngine class to handle email sending functionality

---
+++ emailConsumer/src/engine/emailEngine.ts
@@ -0,0 +1,44 @@
+import { Resend } from "resend";
+import "dotenv/config";
+import { SendEmail } from "../../index";
+
+class EmailEngine {
+  private static instance: EmailEngine;
+  private resend: Resend;
+
+  private constructor() {
+    this.resend = new Resend(process.env.RESEND_API_KEY || "");
+  }
+
+  public static getInstance(): EmailEngine {
+    if (!EmailEngine.instance) {
+      EmailEngine.instance = new EmailEngine();
+    }
+
+    return EmailEngine.instance;
+  }
+
+  public async sendEmail({
+    subject,
+    htmlContent,
+    recipientName,
+    recipientEmail,
+  }: SendEmail): Promise<void> {
+    try {
+      const response = await this.resend.emails.send({
+        from: `${process.env.SENDER_NAME} <${process.env.SENDER_EMAIL}>`,
+        to: recipientEmail,
+        subject: subject,
+        html: htmlContent,
+      });
+
+      console.log("Email sent successfully:", response);
+    } catch (error) {
+      console.error("Error sending email:", error);
+    }
+  }
+}
+
+const emailEngine = EmailEngine.getInstance();
+
+export { emailEngine };
